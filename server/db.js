d6d122c561ee17b0008a9614e8dae5cc94d74c33
Srinithi Ramachandran <60716676+srinithi95@users.noreply.github.com>
2021-03-13 15:57:42 -0800

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mysql = require ('mysql');
  const con = mysql.createPool({
     host:"127.0.0.1",
     user:"root",
-    password:"Saibabaisgr8*",
+    password:"",
     database:"tool-share"
 })
 exports.con=con
